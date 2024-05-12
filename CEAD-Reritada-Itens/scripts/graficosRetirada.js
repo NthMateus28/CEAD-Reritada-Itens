0aa1977e8a25272062b7564d916dca4a17d70e8e
Mateus Loeser Reisdorfer <79878580+NthMateus28@users.noreply.github.com>
2024-05-12 12:22:49 -0300

    Update graficosRetirada.js

--- CEAD-Reritada-Itens/scripts/graficosRetirada.js
+++ CEAD-Reritada-Itens/scripts/graficosRetirada.js
@@ -28,7 +28,10 @@ let quantidades = {
     Colchao: 0,
     roupaIntima: 0,
     kitLimpeza: 0,
-    Toalha: 0
+    Toalha: 0,
+    Fralda: 0,
+    Cobertor: 0,
+    Travesseiro: 0
 };
 
 // Referência à lista de desabrigados
@@ -55,8 +58,17 @@ function atualizarGrafico() {
                     label: "Quantidade de Itens Retirados",
                     data: Object.values(quantidades),
                     backgroundColor: [
-                        '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40'
+                        '#FF6384', // Rosa
+                        '#36A2EB', // Azul
+                        '#FFCE56', // Amarelo
+                        '#4BC0C0', // Verde-água
+                        '#9966FF', // Roxo
+                        '#FF9F40', // Laranja
+                        '#C9CBCF', // Cinza
+                        '#7BC043', // Verde claro
+                        '#F7464A'  // Vermelho
                     ]
+                    
                 }]
             },
             options: {
